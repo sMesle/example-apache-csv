8a48e37b5be5370c0ee00f3190fe580a49d2fa68
sMesle <steve.demeulemeester@efrei.net>
2019-07-10 21:08:41 +0200

    Add JUnit shouldParseFileToStream()

--- src/test/java/org/steve/StudentCsvParserTest.java
+++ src/test/java/org/steve/StudentCsvParserTest.java
@@ -1,10 +1,14 @@
 package org.steve;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 public class StudentCsvParserTest {
 
@@ -12,20 +16,35 @@ public class StudentCsvParserTest {
     private List<StudentDTO> studentDTO;
     private String fileName;
 
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         parser = new StudentCsvParserImpl();
         this.fileName = "sample-data.csv";
+        this.studentDTO = parser.parseCsv(fileName);
     }
 
     @Test
     public void testParseCsv() {
         try {
-            this.studentDTO = parser.parseCsv(fileName);
             parser.printCsv(this.studentDTO);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @Test
+    public void shouldParseFileToStream() {
+        StudentDTO firstElement = this.studentDTO.get(0);
+        StudentDTO elementTest = new StudentDTO();
+        elementTest.setStudentId(1);
+        elementTest.setFirstName("Michael");
+        elementTest.setLastName("Fox");
+        elementTest.setMiddleName("J");
+
+        assertEquals(Arrays.asList(firstElement.getStudentId(), firstElement.getFirstName(), firstElement.getLastName(),
+                firstElement.getMiddleName()),
+                Arrays.asList(elementTest.getStudentId(), elementTest.getFirstName(), elementTest.getLastName(),
+                        elementTest.getMiddleName()));
+    }
 }
